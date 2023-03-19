f21a0e9e0ed9b4940213c0ad8bdd71065b28ca3d
jpskonee <emmanuel.agho@bnkbl.com>
2023-03-19 23:35:55 +0000

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const index = () => {
-  return <h1 className='text-6xl text-tfn-green flex justify-center items-center h-screen'>TFN Alumni Portal</h1>;
+  return <h1 className='text-6xl text-tfn-green flex justify-center items-center h-screen'>TFN Alumni Portal- </h1>;
 };
 
 export default index;
