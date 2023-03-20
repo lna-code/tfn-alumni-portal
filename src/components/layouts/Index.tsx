53f2923bea0f5f19fa27674dd4163a17d11051a2
jpskonee <emmanuel.agho@bnkbl.com>
2023-03-20 07:49:35 +0000

    fix

--- src/components/layouts/Index.tsx
+++ src/components/layouts/Index.tsx
@@ -34,6 +34,8 @@ const Index = ({ children }: IndexProps) => {
         return null;
     }
   } else {
+    console.log(pathname)
+    console.log(PageEndpoints.profile)
     switch (pathname) {
       case PageEndpoints.home:
         return <div>{children}</div>;
