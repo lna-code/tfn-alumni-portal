100f22ac98c571e8d2592d9d11ddda75941f7fc1
jpskonee <emmanuel.agho@bnkbl.com>
2023-03-23 21:04:51 +0000

    adding auth layout

--- src/components/layouts/AuthLayout/AuthLayout.tsx
+++ src/components/layouts/AuthLayout/AuthLayout.tsx
@@ -10,7 +10,7 @@ const AuthLayout = ({ children }: AuthLayoutProps): JSX.Element => {
   return (
     <div className='relative flex justify-center items-center lg:flex-row flex-col-reverse'>
       <div className='absolute lg:top-6 md:top-4 top-2 lg:left-6 md:left-4 left-2 z-20'>
-        <Image src={tfnLogo.src} className='md:h-12 ' alt='tfn logo' />
+        <Image src={tfnLogo.src} className='md:h-12' width={200} height={100} alt='tfn logo' />
       </div>
       <div className='lg:w-[65%] w-full lg:h-screen lg:px-0 px-5 lg:relative absolute lg:top-0 top-[21rem]'>
         <div className='flex justify-center items-center flex-col lg:min-h-screen min-h-[500px] bg-tfn-powder shadow-xl lg:rounded-none rounded-3xl'>{children}</div>
