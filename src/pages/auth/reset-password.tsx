f3949f7b2f3a15d4a1935e452e065e0e85d48f42
sultyl <sultyleke@gmail.com>
2023-04-02 00:07:15 +0100

    Designed a reset password screen

--- src/pages/auth/reset-password.tsx
+++ src/pages/auth/reset-password.tsx
@@ -9,26 +9,36 @@ const ResetPassword = () => {
     <div>
       <div className='text-center'>
         <h1 className='text-tfn-green font-bold text-2xl'>Password Reset</h1>
-        <p className='text-[#989797] text-base leading-5 mt-4'>We sent an email to <b className='text-[#121212]'>tadealumni@gmail.com</b></p>
-        <form action="#" className='mt-10 mb-5'>
-          <input type="tel" className='h-[72px] w-[65px] rounded-lg align-center mr-5  shadow-2xl text-[40px] text-center font-bold' maxLength={1}/>
-          <input type="tel" className='h-[72px] w-[65px] rounded-lg align-center mr-5 shadow-2xl text-[40px] text-center font-bold ' maxLength={1}/>
-          <input type="tel" className='h-[72px] w-[65px] rounded-lg align-center mr-5 shadow-2xl text-[40px] text-center font-bold ' maxLength={1}/>
-          <input type="tel" className='h-[72px] w-[65px] rounded-lg align-center shadow-2xl text-[40px] text-center font-bold' maxLength={1}/>
+        <p className='text-[#989797] text-base leading-5 mt-4'>
+          We sent an email to <b className='text-[#121212]'>tadealumni@gmail.com</b>
+        </p>
+        <form action='#' className='mt-10 mb-5'>
+          <input type='tel' className='h-[72px] w-[65px] rounded-lg align-center mr-5  shadow-2xl text-[40px] text-center font-bold' maxLength={1} />
+          <input type='tel' className='h-[72px] w-[65px] rounded-lg align-center mr-5 shadow-2xl text-[40px] text-center font-bold ' maxLength={1} />
+          <input type='tel' className='h-[72px] w-[65px] rounded-lg align-center mr-5 shadow-2xl text-[40px] text-center font-bold ' maxLength={1} />
+          <input type='tel' className='h-[72px] w-[65px] rounded-lg align-center shadow-2xl text-[40px] text-center font-bold' maxLength={1} />
           <div>
-          <Link href="/">
-          <Button className='w-[100%] 2xl:w-[438px] md:w-[438px] h-[52px] rounded-lg mt-8'>Continue</Button>
-          </Link>
+            <Link href='/'>
+              <Button className='w-[100%] 2xl:w-[438px] md:w-[438px] h-[52px] rounded-lg mt-8'>Continue</Button>
+            </Link>
           </div>
         </form>
         <div className='text-[#989797] '>
-        <p className='sm:hidden'>Didn't recieve the mail? <a href="/" className='font-bold'>Click here to proceed</a></p>
-        <p className='mt-5'><a href="/"><FontAwesomeIcon icon={faArrowLeft} className='mr-6 font-thin'/><b>Back to sign in</b></a></p>
+          <p className='sm:hidden'>
+            Didn't recieve the mail?{' '}
+            <a href='/' className='font-bold'>
+              Click here to proceed
+            </a>
+          </p>
+          <p className='mt-5'>
+            <a href='/'>
+              <FontAwesomeIcon icon={faArrowLeft} className='mr-6 font-thin' />
+              <b>Back to sign in</b>
+            </a>
+          </p>
         </div>
       </div>
-      <div>
-
-      </div>
+      <div></div>
     </div>
   );
 };
